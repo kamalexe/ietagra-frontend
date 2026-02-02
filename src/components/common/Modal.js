53575b8abfc7367f38e66d1f4818bc9b275c1e6c
Kamal Maurya <83895579+kamalexe@users.noreply.github.com>
2026-02-02 23:07:33 +0530

    Update Modal.js

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -24,6 +24,7 @@ const Modal = ({ isOpen = false, onClose, title, children, maxWidth = 'max-w-lg'
                             as={Fragment}
                             enter="ease-out duration-300"
                             enterFrom="opacity-0 scale-95"
+                            
                             enterTo="opacity-100 scale-100"
                             leave="ease-in duration-200"
                             leaveFrom="opacity-100 scale-100"
